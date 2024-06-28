66ee3294434e4adcbdfddf1b7b6c051fcd284c88
CaptainCronch <62529272+CaptainCronch@users.noreply.github.com>
2024-06-28 01:21:00 -0600

    first commit

---
+++ src/routes/(main)/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import '$lib/css/styles.css';
+</script>
+
+<svelte:head>
+	<title>paprika.live</title>
+	<meta name="description" content="Home page" />
+</svelte:head>
+
+<h1>hey gang 🐇</h1>
+
+<style>
+	/* :global(body) {
+		background-image: linear-gradient(90deg, rgba(227,227,202,0.5) 67%, rgba(39,35,32,0.5) 100%);
+		min-height: 100vh;
+		margin: 0;
+	} */
+
+	h1 {
+		margin-top: 0px;
+	}
+</style>
