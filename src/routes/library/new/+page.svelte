1723b697d3cb7ea13f58092552a4964e8b89f2e6
CaptainCronch <62529272+CaptainCronch@users.noreply.github.com>
2024-09-15 01:36:09 -0600

    info frame component

--- src/routes/library/new/+page.svelte
+++ src/routes/library/new/+page.svelte
@@ -2,10 +2,6 @@
 	import ArticleFrame from "./ArticleFrame.svelte";
 	import InfoFrame from "./InfoFrame.svelte";
 	export let data;
-    let author = {name: "cronch", id: 0}
-    let title = "Name"
-    let time = {creation: "12", edited: "15"}
-    let tags = [{name: "epic", id: 0}, {name: "epic", id: 0}]
 </script>
 
 <svelte:head>
@@ -16,10 +12,9 @@
 <ArticleFrame>
     <h1>Welcome to the Library!</h1>
     <p>Here you can find all kinds of articles, from blog posts to personal articles, and from notes to wiki pages.</p>
-    <br>
 </ArticleFrame>
 
-<InfoFrame {author} {title} {time} {tags}/>
+<InfoFrame edit/>
 
 <style>
 	h1 {
--- src/routes/library/new/InfoFrame.svelte
+++ src/routes/library/new/InfoFrame.svelte
@@ -1,35 +1,122 @@
 <script>
-    export let author
-    export let title
-    export let time
-    export let tags
+	import { onMount } from "svelte";
+
+    export let author = {id: 0, name: "you"}
+    export let title = "New Page"
+    export let time = {creation: new Date().toISOString(), edited: new Date().toISOString()}
+    export let tags = [{id: 0, name: "new"}]
+	export let edit = false
+
+	/** @type Element */
+	let infoTags
+	/** @type HTMLCollection */
+	let tagList
+	onMount(() => {
+		infoTags = document.getElementById("info-tags")
+		tagList = infoTags.getElementsByClassName("tag-container") // does not include the add button
+	})
+
+	function handleTitleInput(event) {
+		title = event.target.textContent
+	}
+
+	let tagElements
+	function handleAddTag(event) {
+		if (document.getElementsByClassName("editing").length > 0) {return} // do nothing if tag already being edited
+
+		if (tagList.length < 1) { // only add comma if there is a previous element
+			let comma = document.createElement("span")
+			comma.textContent = ", "
+			tagList.item(tagList.length - 1).appendChild(comma)
+		}
+
+		let tagContainer = document.createElement("span")
+		tagContainer.className = "tag-container"
+
+		tagList.item(tagList.length - 1).insertAdjacentElement("afterend", tagContainer)
+
+		let tag = document.createElement("a")
+		tag.className = "tag editing" // .editing to show the editable tag
+		tag.href = "#"
+		tag.contentEditable = true
+		tag.textContent = "#new tag"
+		tag.addEventListener("keydown", handleTagEnter)
+
+		tagContainer.appendChild(tag)
+	}
+	/** @param {KeyboardEvent} event */
+	function handleTagEnter(event) {
+		if (event.key === "Enter") {
+			event.preventDefault()
+
+			// let tag
+			// tag.className = "tag"
+			// tag.addEventListener("click", handleDeleteTag)
+		}
+	}
+
+	function handleDeleteTag(event) {
+		event.preventDefault()
+	}
 </script>
 
 <div class="info-frame">
 	<div class="bolt"></div>
 	<aside>
-		<p id="info-author"><a class="tag" href="/library/user/{author.id}">@{author.name}</a></p>
-		<h2 id="info-title">{title}</h2>
+		<p id="info-author"><a class="tag" href="{edit ? "/library/new" : "/library/user/" + author.id}">@{author.name}</a></p>
+		<h2 id="info-title" contenteditable={edit} on:input={handleTitleInput}>{title}</h2>
 		<p id="info-subtitle"><span id="info-time" title="last modified {time.edited}">{time.creation}</span></p>
 		<p>HTML; text on canvas</p>
 		<br>
-		<p id="info-tags">
+		<p id="info-tags" bind:this={infoTags}>
             {#each tags as tag, index}
-			<a class="tag" href="/library/tag/{tag.id}">#{tag.name}</a>{#if index !== tags.length - 1}<span>, </span>{/if}
+				<span class="tag-container">
+					<a class="tag" href={edit ? "#" : "/library/tag/" + tag.id} on:click={handleDeleteTag}>#{tag.name}</a>{#if index !== tags.length - 1}<span>, </span>{/if}
+				</span>
             {/each}
+			{#if edit}<button class="add" on:click={handleAddTag}>+</button>{/if}
 		</p>
 	</aside>
 	<div class="bolt"></div>
 </div>
 
 <style>
+	:global(.tag.editing) {
+		min-width: 25px;
+		border: 2px solid var(--black);
+		padding: 5px;
+	}
+
+	.tag-container {
+		margin: 0;
+		padding: 0;
+	}
+
+	#info-tags {
+		cursor: default;
+	}
+
+	.add {
+		padding: 0;
+		background: none;
+		border: none;
+		cursor: pointer;
+		font-weight: 700;
+		vertical-align: top;
+		color: var(--moss-green);
+		margin-left: 10px;
+		scale: 1.5;
+	}
+
     aside p {
 		margin: 10px 0 0 0;
 	}
 
-	.tag {
+	:global(.tag) {
+		margin: 0;
+		padding: 0;
 		text-decoration: underline;
-		color: inherit;
+		color: var(--black);
 	}
 
 	#info-author {
@@ -58,6 +145,7 @@
 		margin: 0;
 		font-size: 1.5em;
 		font-weight: 800;
+		overflow-wrap: break-word;
 	}
 
 	aside {
